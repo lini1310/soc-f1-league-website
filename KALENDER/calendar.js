a65d4abe61d8471e03bcf18955d5f37547291b92
Koopro <90864805+Koopro@users.noreply.github.com>
2023-03-26 19:15:47 +0200

    Calnedar added, Home edited

--- KALENDER/calendar.js
+++ KALENDER/calendar.js
@@ -1,117 +1,80 @@
-// Replace with your iCloud calendar's public URL
-const ICLOUD_CALENDAR_URL = 'https://pXX-calendars.icloud.com/published/2/your_unique_calendar_id_here';
+const currentDate = new Date();
+let currentMonth = currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
 
-// Generate the calendar structure
-function createCalendar(year, month) {
-  const firstDay = new Date(year, month).getDay();
-  const daysInMonth = 32 - new Date(year, month, 32).getDate();
+const monthYearElement = document.getElementById("month-year");
+const calendarDaysElement = document.querySelector(".calendar-days");
 
-  let calendarHTML = '<table id="calendar"><thead><tr>';
-
-  // Weekdays
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  for (let day of days) {
-    calendarHTML += `<th>${day}</th>`;
+function displayCalendar(month, year) {
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const firstDayOfMonth = new Date(year, month, 1).getDay();
+  const lastDayOfMonth = new Date(year, month, daysInMonth).getDay();
+  
+  monthYearElement.textContent = `${getMonthName(month)} ${year}`;
+  
+  let days = "";
+  for (let i = 1; i <= daysInMonth; i++) {
+    days += `<div class="${getDateClass(i, month, year)}">${i}</div>`;
   }
-
-  calendarHTML += '</tr></thead><tbody><tr>';
-
-  // Fill in the days
-  for (let i = 1; i <= 42; i++) {
-    const day = i - firstDay;
-    if (i % 7 === 1) {
-      calendarHTML += '</tr><tr>';
-    }
-
-    if (day < 1 || day > daysInMonth) {
-      calendarHTML += '<td></td>';
-    } else {
-      calendarHTML += `<td data-date="${year}-${(month + 1).toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}">${day}</td>`;
-    }
+  calendarDaysElement.innerHTML = days;
+  
+  let prevMonthDays = "";
+  for (let i = firstDayOfMonth - 1; i >= 0; i--) {
+    const date = new Date(year, month, -i).getDate();
+    prevMonthDays += `<div class="prev-month">${date}</div>`;
   }
-
-  calendarHTML += '</tr></tbody></table>';
-  document.getElementById('calendar').innerHTML = calendarHTML;
+  calendarDaysElement.innerHTML = prevMonthDays + days;
+  
+  let nextMonthDays = "";
+  for (let i = 1; i < 7 - lastDayOfMonth; i++) {
+    const date = new Date(year, month + 1, i).getDate();
+    nextMonthDays += `<div class="next-month">${date}</div>`;
+  }
+  calendarDaysElement.innerHTML += nextMonthDays;
 }
 
-// Fetch events from the iCloud calendar
-async function fetchEvents() {
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-  const response = await fetch(proxyUrl + ICLOUD_CALENDAR_URL);
-  const data = await response.text();
-  const parser = new DOMParser();
-  const xmlDoc = parser.parseFromString(data, 'text/xml');
-  const events = xmlDoc.getElementsByTagName('VEVENT');
-
-  for (const event of events) {
-    const summary = event.querySelector('SUMMARY').textContent;
-    const startDate = event.querySelector('DTSTART').textContent.slice(0, 8);
-    const formattedDate = `${startDate.slice(0, 4)}-${startDate.slice(4, 6)}-${startDate.slice(6, 8)}`;
-
-    const eventElement = document.querySelector(`[data-date="${formattedDate}"]`);
-    if (eventElement) {
-      eventElement.classList.add('event');
-      eventElement.setAttribute('title', summary);
-
-      // Create a tooltip for the event
-      const tooltip = document.createElement('div');
-      tooltip.classList.add('tooltip');
-      const tooltipText = document.createElement('span');
-      tooltipText.classList.add('tooltip-text');
-      tooltipText.textContent = summary;
-      tooltip.appendChild(tooltipText);
-      eventElement.appendChild(tooltip);
-    }
+function getDateClass(day, month, year) {
+  const date = new Date(year, month, day);
+  if (date.toDateString() === currentDate.toDateString()) {
+    return "today";
   }
+  return "";
 }
 
-// Initialize the calendar
-const today = new Date();
-createCalendar(today.getFullYear(), today.getMonth());
-fetchEvents();
+function getMonthName(month) {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return months[month];
+}
 
-// Add navigation controls
-const prevMonthBtn = document.createElement('button');
-prevMonthBtn.textContent = 'Previous Month';
-const nextMonthBtn = document.createElement('button');
-nextMonthBtn.textContent = 'Next Month';
-const btnGroup = document.createElement('div');
-btnGroup.classList.add('btn-group');
-btnGroup.appendChild(prevMonthBtn);
-btnGroup.appendChild(nextMonthBtn);
-document.getElementById('calendar').insertAdjacentElement('afterend', btnGroup);
+displayCalendar(currentMonth, currentYear);
 
-let currentYear = today.getFullYear();
-let currentMonth = today.getMonth();
-prevMonthBtn.addEventListener('click', () => {
-  if (currentMonth === 0) {
-    currentYear--;
+document.getElementById("prev").addEventListener("click", () => {
+  currentMonth--;
+  if (currentMonth < 0) {
     currentMonth = 11;
-  } else {
-    currentMonth--;
+    currentYear--;
   }
-// Update the calendar when navigating
-nextMonthBtn.addEventListener('click', () => {
-    if (currentMonth === 11) {
-    currentYear++;
+  displayCalendar(currentMonth, currentYear);
+});
+
+document.getElementById("next").addEventListener("click", () => {
+  currentMonth++;
+  if (currentMonth > 11) {
     currentMonth = 0;
-    } else {
-    currentMonth++;
-    }
-    
-    createCalendar(currentYear, currentMonth);
-    fetchEvents();
-    });
-    
-    // Update the calendar when navigating
-    prevMonthBtn.addEventListener('click', () => {
-    if (currentMonth === 0) {
-    currentYear--;
-    currentMonth = 11;
-    } else {
-    currentMonth--;
-    }
-    
-    createCalendar(currentYear, currentMonth);
-    fetchEvents();
-    });
+    currentYear++;
+  }
+  displayCalendar(currentMonth, currentYear);
+});
